d02f5092d6836bb400b815ce3c55f1a5910364b1
izmebalu <44422588+izmebalu@users.noreply.github.com>
2022-04-26 21:50:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket_coach"
+    bucket = "mydev-tf-state-bucket-coach"
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
